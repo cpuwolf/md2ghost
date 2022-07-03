3e1e1fa23e68a8795b7bc24747ade0aac8190f90
Wei Shuai <cpuwolf@sina.com>
2022-07-03 09:25:19 +0800

    Initial Commit for ghost v5

---
+++ postghost.py
@@ -0,0 +1,62 @@
+
+# written by Wei Shuai <cpuwolf@gmail.com> July 1st, 2022
+
+import requests # pip install requests
+import jwt	# pip install pyjwt
+from datetime import datetime as date
+import json
+
+import os
+
+
+with open(".config.json", "r") as f:
+    ghost_config = json.load(f)
+    base_url=ghost_config['base_url']
+    admin_key=ghost_config['admin_key']
+    
+
+# Admin API key goes here
+print(admin_key)
+
+# Split the admin_key into ID and SECRET
+id, secret = admin_key.split(':')
+
+# Prepare header and payload
+iat = int(date.now().timestamp())
+
+header = {'alg': 'HS256', 'typ': 'JWT', 'kid': id}
+payload = {
+    'iat': iat,
+    'exp': iat + 5 * 60,
+    'aud': '/admin/'
+}
+
+
+# Create the token (including decoding secret)
+token = jwt.encode(payload, bytes.fromhex(secret), algorithm='HS256', headers=header)
+
+print(token)
+
+if os.name == 'nt':
+    print('Ghost {}'.format(token))
+else:
+    print('Ghost {}'.format(token.decode("utf-8")))
+
+# Make an authenticated request to create a post
+url = base_url + 'ghost/api/admin/posts/'
+if os.name == 'nt':
+    headers = {'Authorization': 'Ghost {}'.format(token)}
+else:
+    headers = {'Authorization': 'Ghost {}'.format(token.decode("utf-8"))}
+body = {
+    "posts": [
+        {
+            "title": "My test post",
+            "mobiledoc": "{\"version\":\"0.3.1\",\"atoms\":[],\"cards\":[],\"markups\":[],\"sections\":[[1,\"p\",[[0,[],0,\"My post content. Work in progress...\"]]]]}",
+            "status": "published"
+        }
+    ]
+}
+r = requests.post(url, json=body, headers=headers)
+
+print(r.content)
---
+++ tools/exec.cmd
@@ -0,0 +1,7 @@
+call env.cmd
+
+::pip install pyjwt
+
+python.exe postghost.py -f D:\works\GitHub\discuzX2hexo\blog\source\_posts\2021-01-16-msfs2020.md
+popd
+pause
